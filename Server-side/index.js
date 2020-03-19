a684a6100a16978f55bf50a5e6cac40cb15dcaf7
pravin9 <pravin9@users.noreply.github.com>
2020-03-20 02:04:51 +0530

    login and signup

---
+++ Server-side/index.js
@@ -0,0 +1,39 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const user = require("./routes/user"); //new addition
+
+
+// Initiate Mongo Server
+const mongoose = require('mongoose'); 
+mongoose.connect('mongodb://localhost:27017/gfg'); 
+var db=mongoose.connection; 
+db.on('error', console.log.bind(console, "connection error")); 
+db.once('open', function(callback){ 
+    console.log("connection succeeded"); 
+}) 
+
+
+const app = express();
+
+// PORT
+const PORT = process.env.PORT || 4000;
+
+// Middleware
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API Working" });
+});
+
+
+/**
+ * Router Middleware
+ * Router - /user/*
+ * Method - *
+ */
+app.use("/user", user);
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server Started at PORT ${PORT}`);
+});
