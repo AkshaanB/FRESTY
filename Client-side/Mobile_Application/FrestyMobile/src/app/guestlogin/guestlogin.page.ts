d6951110184cc6c3b8c3a41f9e299125f4a65fe3
Dimithra Bandara <DimithraB@gmail.com>
2020-03-22 18:19:28 +0530

    added validation for signup,signin and added camera function

---
+++ Client-side/Mobile_Application/FrestyMobile/src/app/guestlogin/guestlogin.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-guestlogin',
+  templateUrl: './guestlogin.page.html',
+  styleUrls: ['./guestlogin.page.scss'],
+})
+export class GuestloginPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
