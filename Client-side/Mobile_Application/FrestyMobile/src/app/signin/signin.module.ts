d98c30fa69e0b727ecfb1ba2b58a97290f1a1a4c
Dimithra Bandara <DimithraB@gmail.com>
2020-04-21 19:27:30 +0530

    Added sqlite, updated validation for signup

--- Client-side/Mobile_Application/FrestyMobile/src/app/signin/signin.module.ts
+++ Client-side/Mobile_Application/FrestyMobile/src/app/signin/signin.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule} from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { SigninPage } from './signin.page';
 
 import { SigninPageRoutingModule } from './signin-routing.module';
 
-import { SigninPage } from './signin.page';
+const routes: Routes = [
+  {
+    path: '',
+    component: SigninPage
+  }
+];
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     SigninPageRoutingModule
   ],
   declarations: [SigninPage]
--- Client-side/Mobile_Application/FrestyMobile/src/app/signin/signin.page.ts
+++ Client-side/Mobile_Application/FrestyMobile/src/app/signin/signin.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninPage implements OnInit {
 
-  constructor() { }
+  credentialsForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) { }
 
   ngOnInit() {
+    this.credentialsForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.email]],     //user name is required 
+      password: ['', [Validators.required, Validators.minLength(6)]]   //password is required with a minimum length of 6 characters
+    });
+  }
+
+  onSubmit(){
+    this.authService.login(this.credentialsForm.value).subscribe();
   }
 
 }
